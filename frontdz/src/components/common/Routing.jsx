9b742071f41426e100be14d6f122bbb618537cc9
Maria Paz Oviedo <mzoviedo@uc.cl>
2024-11-24 20:33:20 -0300

    Editar Perfil

--- frontdz/src/components/common/Routing.jsx
+++ frontdz/src/components/common/Routing.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Rules from '../game/Rules';
 import Profile from '../profile/Profile';
+import EditProfile from '../profile/EditProfile';
 import NewGame from '../game/NewGame';
 import NewReport from '../profile/NewReport';
 import Cards from '../game/Cards';
@@ -28,6 +29,7 @@ function Routing() {
                 <Route path="/" element={<App />} />
                 <Route path="/rules" element={<Rules />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/editprofile" element={<EditProfile />} />
                 <Route path="/newgame" element={<NewGame />} />
                 <Route path="/newreport" element={<NewReport />} />
                 <Route path="/cards" element={<Cards />} />
